ae1c08aa7236db7d5d1d9602f1b6d3d487671685
munizimpacta <126532973+munizimpacta@users.noreply.github.com>
2023-04-17 21:50:46 -0300

    Update primos.py

--- primos.py
+++ primos.py
@@ -16,7 +16,7 @@ def nao_entre_em_panico():
         if total == 2:
             primos.append(numero)
         numero += 1
-    return primos0
+    return primos
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
